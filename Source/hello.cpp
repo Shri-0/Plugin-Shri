07746ffabfc4d23f36989c50663b81f35fbb0813
Shri <shrinath.narayanan@gmail.com>
2023-09-28 19:42:03 -0700

    "testing c++ file"

--- Source/hello.cpp
+++ Source/hello.cpp
@@ -1,1 +1,14 @@
-test test
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	cout << "No "
+		 << "change "
+		 << "here" << endl;
+	cout << "Shift some text " << setw(25) << "to the right" << endl;
+	cout << setw(50) << "Or shift it all" << endl;
+	return 0;
+}
