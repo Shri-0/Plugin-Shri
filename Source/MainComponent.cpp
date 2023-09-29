6f47e8607bd75c9da9020ba4b5d393ee3a7ce062
Shri <shrinath.narayanan@gmail.com>
2023-09-28 19:33:59 -0700

    Plugin

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,17 @@
+#include "MainComponent.h"
+
+MainComponent::MainComponent()
+{
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+void MainComponent::paint(juce::Graphics &)
+{
+}
+
+void MainComponent::resized()
+{
+}
